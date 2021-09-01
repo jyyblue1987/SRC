4fa20bee135170cd8b12aa006f2853aac505210a
jyyblue <jyyblue1987@outlook.com>
2021-09-01 10:53:18 +0800

    [fixed] fix severity.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -306,7 +306,7 @@ export default function Home() {
                                             </div>
                                             <div class="md:w-2/3">
                                                 <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" 
-                                                    value={allergy['severity']}  onChange={(event) => onChangeAllegry(event, 'liseverityne1')}/>
+                                                    value={allergy['severity']}  onChange={(event) => onChangeAllegry(event, 'severity')}/>
                                             </div>
                                         </div> 
 
